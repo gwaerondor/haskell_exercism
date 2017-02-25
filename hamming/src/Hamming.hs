e906f308c1cb592a8b6b152fb681afa070be8431
Robin Larsson <gwaerondor@gmail.com>
2017-02-25 10:56:47 +0100

    Updates the solution for Hamming

--- hamming/src/Hamming.hs
+++ hamming/src/Hamming.hs
@@ -1,14 +1,9 @@
 module Hamming (distance) where
 
-distance :: String -> String -> Maybe Integer
+distance :: String -> String -> Maybe Int
 distance xs ys
-  | sameLength = Just (distance' xs ys)
+  | sameLength = Just $ length $ filter (\(x, y) -> x /= y) (zip xs ys)
   | otherwise = Nothing
   where
     sameLength = (length xs) == (length ys)
 
-distance' :: String -> String -> Integer
-distance' "" "" = 0
-distance' (x:xs) (y:ys)
-  | x == y = 0 + (distance' xs ys)
-  | otherwise = 1 + (distance' xs ys)

663cdf4da119250fdc2be282934355e66d4f47bf
gwaerondor <gwaerondor@gmail.com>
2017-05-30 13:59:15 +0200

    Adds full solution of all your base

--- all-your-base/src/Base.hs
+++ all-your-base/src/Base.hs
@@ -1,14 +1,28 @@
 module Base (rebase, toDecimal) where
 
 rebase :: Integral a => a -> a -> [a] -> Maybe [a]
-rebase inputBase outputBase inputDigits = Just [1]
-
+rebase inputBase outputBase inputDigits
+  | hasInvalidBase = Nothing
+  | otherwise = do
+      decimal <- (toDecimal inputBase inputDigits)
+      Just (fromDecimal outputBase decimal)
+  where hasInvalidBase = any (<= 1) [inputBase, outputBase]
+    
 toDecimal :: Integral a => a -> [a] -> Maybe a
 toDecimal inputBase digits
+  | inputBase < 2 = Nothing
   | validNumber = Just (toDecimal' inputBase digits (length digits))
   | otherwise = Nothing
   where
-    validNumber = all (<inputBase) digits
+    validNumber = (all (<inputBase) digits) && (all (>= 0) digits)
 
+toDecimal' :: Integral a => a -> [a] -> Int -> a
 toDecimal' _ [] _ = 0
 toDecimal' inputBase (d:ds) pos = (d * (inputBase^(pos-1))) + (toDecimal' inputBase ds (pos-1))
+
+fromDecimal :: Integral a => a -> a -> [a]
+fromDecimal outputBase dec = reverse $ fromDecimal' outputBase dec
+
+fromDecimal' :: Integral a => a -> a -> [a]
+fromDecimal' _ 0 = []
+fromDecimal' outputBase dec = (dec `rem` outputBase):(fromDecimal' outputBase (dec `quot` outputBase))

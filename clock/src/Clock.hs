09f451e88bfc7420f28beb56dd028fe009f45317
gwaerondor <gwaerondor@gmail.com>
2017-07-04 14:23:11 +0200

    Refactors clock

--- clock/src/Clock.hs
+++ clock/src/Clock.hs
@@ -1,31 +1,24 @@
-module Clock (clockHour, clockMin, fromHourMin, toString) where
+module Clock (fromHourMin, toString) where
 
 import Text.Printf (printf)
 
 data Clock = Clock Integer deriving (Show, Eq)
 
 instance Num Clock where
-  fromInteger m = fromHourMin 0 (m `mod` day)
+  fromInteger m = fromHourMin 0 (wrap m)
   negate (Clock m) = Clock (day - m)
   (+) (Clock m1) (Clock m2) = fromInteger (m1 + m2)
 
-clockHour :: Clock -> Integer
-clockHour (Clock minutes) = first $ (minutes `divMod` hour)
-  where
-    first (f, _) = f
-    
-clockMin :: Clock -> Integer
-clockMin (Clock m) = second $ (m `divMod` hour)
-  where
-    second (_, s) = s
-
 fromHourMin :: Integer -> Integer -> Clock
-fromHourMin h m = Clock (((h*60) + m) `mod` day)
+fromHourMin h m = Clock $ wrap ((h*60) + m)
 
 toString :: Clock -> String
 toString (Clock m) = printf "%02d:%02d" hours minutes
   where
-    (hours, minutes) = (m `divMod` hour)
+    (hours, minutes) = m `divMod` hour
+
+wrap :: Integer -> Integer
+wrap minutes = minutes `mod` day
 
 hour :: Integer
 hour = 60

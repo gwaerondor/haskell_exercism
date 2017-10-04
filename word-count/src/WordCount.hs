1dd016bb539a0e9e2610edd34d99c7cffb88f27c
gwaerondor <gwaerondor@gmail.com>
2017-10-04 12:53:36 +0200

    Adds full solution to word count

--- word-count/src/WordCount.hs
+++ word-count/src/WordCount.hs
@@ -1,8 +1,11 @@
 module WordCount (wordCount) where
-import Data.String (words)
+import Data.Char (toLower)
 
 wordCount :: String -> [(String, Int)]
-wordCount xs = count [] $ words xs
+wordCount xs = count [] $ map normalize $ splitWords xs
+
+normalize :: String -> String
+normalize cs = removeWrappingQuotes $ lower cs
 
 count :: Eq a => [(a, Int)] -> [a] -> [(a, Int)]
 count acc [] = acc
@@ -16,3 +19,22 @@ update :: Eq a => a -> b -> [(a, b)] -> [(a, b)]
 update key newValue ((x, oldValue):xs)
   | x == key = (x, newValue):xs
   | otherwise = (x, oldValue):(update key newValue xs)
+
+splitWords :: String -> [String]
+splitWords [] = []
+splitWords sentence = nextWord:(splitWords remainder)
+  where
+    nextWord = (takeWhile isWord sentence)
+    remainder = dropWhile isSpecial $ dropWhile isWord sentence
+    isWord x = not $ isSpecial x
+    isSpecial x = x `elem` special
+    special = ", !@%^&\n.$:"
+
+lower :: String -> String
+lower = map toLower
+
+removeWrappingQuotes :: String -> String
+removeWrappingQuotes cs =
+  let isWrapped = ((head cs) == '\'') && ((last cs) == '\'') in
+  case isWrapped of True -> tail $ init cs
+                    False -> cs
